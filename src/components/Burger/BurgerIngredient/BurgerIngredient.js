8f69c3ed5956f66d69fc1d8409640629ce3e9d6c
unknown <batuhan.balaban@yahoo.com>
2019-08-12 22:49:23 +0300

    BurgerIngredient component added

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import classes from './BurgerIngredients.css'
+
+const burgerIngredient = (props) =>{
+
+    let ingredient = null;
+
+    switch(props.type){
+        case ('bread-bottom'):
+            ingredient = <div className = {classes.BreadBottom}></div>;
+            break;
+        case ('bread-bottom'):
+            ingredient = (
+            <div className = {classes.BreadTop}>
+                <div className = {classes.Seeds1}></div>
+                <div className = {classes.Seeds2}></div>
+            </div>);
+            break;
+        case ('meat'):
+                ingredient = <div className = {classes.Meat}></div>;
+                break;
+        case ('cheese'):
+                ingredient = <div className = {classes.Cheese}></div>;
+                break;
+        case ('bacon'):
+                ingredient = <div className = {classes.Bacon}></div>;
+                break;
+        case ('salad'):
+                ingredient = <div className = {classes.Salad}></div>;
+                break;
+        default:
+                ingredient = null;
+                break;
+    }
+
+    return ingredient;
+
+};
+
+export default burgerIngredient;
