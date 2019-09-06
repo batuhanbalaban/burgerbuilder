481f7102904dd91127863db3b3c1c8eab2b65368
unknown <batuhan.balaban@yahoo.com>
2019-09-06 19:30:49 +0300

    Post order to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-order';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -93,7 +94,24 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        //alert('You continue!');
+        const order ={
+            ingredients: this.state.ingredients,
+            price:this.state.price,
+            customer:{
+                name:'Batuhan Balaban',
+                address:{
+                    street:'blabla st',
+                    zipCode:'34000',
+                    country:'Germany'
+                },
+                email:'test@test.com'
+            },
+            deliveryMethod:'fastest'
+        }
+        axios.post('/orders.json',order)
+        .then(response=>console.log(response))
+        .catch(error=>console.log(error));
     }
 
     render (){
