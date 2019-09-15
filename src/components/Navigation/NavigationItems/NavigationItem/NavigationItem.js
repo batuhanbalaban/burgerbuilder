3780cfdf419844af0c5d14ac1fffdbaf457e010e
unknown <batuhan.balaban@yahoo.com>
2019-09-15 11:24:46 +0300

    Orders Container  added

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './NavigationItem.css'
+import {NavLink} from 'react-router-dom';
 
 const navigationItem = (props) =>(
         <li className={classes.NavigationItem}>
-            <a 
-            href={props.link}
-            className={props.active ? classes.active: null}>
+            <NavLink exact
+            to={props.link} 
+            activeClassName={classes.active}>
                 {props.children}
-            </a>
+            </NavLink>
         </li>
 );
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,8 +4,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) =>(
     <ul className={classes.NavigationItems}>
-        <NavigationItem link='/' active={true}>Burger Builder</NavigationItem>
-        <NavigationItem link='/' >Checkout</NavigationItem>
+        <NavigationItem link='/'>Burger Builder</NavigationItem>
+        <NavigationItem link='/orders' >Orders</NavigationItem>
     </ul>
 );
 
