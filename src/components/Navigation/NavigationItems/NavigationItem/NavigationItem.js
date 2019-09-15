22ae472678d7adfdc3dd9ada41306149589b1373
unknown <batuhan.balaban@yahoo.com>
2019-09-15 12:53:16 +0300

    orders fetched from firebase

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom';
 
 const navigationItem = (props) =>(
         <li className={classes.NavigationItem}>
-            <NavLink exact
+            <NavLink exact={props.exact}
             to={props.link} 
             activeClassName={classes.active}>
                 {props.children}
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,17 +1,44 @@
 import React, {Component} from 'react';
 import Order from '../../components/Order/Order';
-
+import axios from '../../axios-order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 class Orders extends Component {
+
+    state = {
+        orders:[],
+        loading:true
+    }
+    componentDidMount(){
+        axios.get('/orders.json')
+        .then(res => {
+            //console.log(res.data);
+            const fetchedOrders=[];
+            for(let key in res.data){
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            this.setState({loading:false,orders:fetchedOrders});
+        })
+        .catch(err=>{
+            this.setState({loading:false});
+        });
+    }
 
     render(){
 
         return(
         <div>
-            <Order/>
-            <Order/>
+            {this.state.orders.map(ord =>(
+                <Order 
+                key = {ord.id}
+                ingredients = {ord.ingredients}
+                price = {ord.price}/>
+            ))}
         </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders,axios);
