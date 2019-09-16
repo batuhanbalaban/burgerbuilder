7bba94a2e2abd9c9a4cbde17958649f75146b3bf
unknown <batuhan.balaban@yahoo.com>
2019-09-16 19:56:52 +0300

    Dynamically Create Inputs based on JS Config

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,15 +5,38 @@ const input = (props) =>{
 
     let inputElement = null;
 
-    switch(props.inputtype){
+    switch(props.elementType){
         case('input'):
-            inputElement=<input className={classes.InputElement} {...props}/>;
+            inputElement=<input 
+                            onChange={props.changed}
+                            className={classes.InputElement} 
+                            {...props.elementConfig} 
+                            value={props.value}/>;
             break;
         case('textarea'):
-            inputElement=<textarea className={classes.InputElement} {...props}/>;
+            inputElement=<textarea 
+                            onChange={props.changed}
+                            className={classes.InputElement} 
+                            {...props.elementConfig} 
+                            value={props.value}/>;
+            break;
+        case('select'):
+            inputElement=(<select 
+                            onChange={props.changed}
+                            className={classes.InputElement} 
+                            value={props.value}>
+                                {props.elementConfig.options.map(option => (
+                                    <option key ={option.value} value ={option.value}  >{option.displayValue}</option>
+                                )) }
+                            </select>
+                            );
             break;
         default:
-            inputElement=<input className={classes.InputElement} {...props}/>;
+            inputElement=<input 
+                            onChange={props.changed}
+                            className={classes.InputElement} 
+                            {...props.elementConfig} 
+                            value={props.value}/>;
             break;
     }
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,11 +7,57 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component{
     state={
-        name:'',
-        email:'',
-        address:{
-            street:'',
-            postalcode:''
+        orderForm: {            
+            name:{
+                    elementType:'input',
+                    elementConfig:{
+                        type:'text',
+                        placeholder:'Your Name'
+                    },
+                    value:''
+                },
+            street:{
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Street'
+                },
+                value:''
+            },
+            zipCode:{
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'ZIP Code'
+                },
+                value:''
+            },
+            country:{
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Country'
+                },
+                value:''
+            },
+            email:{
+                elementType:'email',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Your E-Mail'
+                },
+                value:''
+            },
+            deliveryMethod:{
+                elementType:'select',
+                elementConfig:{
+                    options:[
+                            {value:'fastest', displayValue:'Fastest'},
+                            {value:'cheapest', displayValue:'Cheapest'}
+                        ]
+                },
+                value:''
+            }
         },
         loading:false
 
@@ -24,17 +70,8 @@ class ContactData extends Component{
         this.setState({loading:true});
         const order ={
             ingredients: this.props.ingredients,
-            price:this.props.price,
-            customer:{
-                name:'Batuhan Balaban',
-                address:{
-                    street:'blabla st',
-                    zipCode:'34000',
-                    country:'Germany'
-                },
-                email:'test@test.com'
-            },
-            deliveryMethod:'fastest'
+            price:this.props.price
+
         }
         axios.post('/orders.json',order)
         .then(response=>{
@@ -48,15 +85,45 @@ class ContactData extends Component{
         });
     }
 
+    inputChangedHandler = (event, inputIdentifier) =>{
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
 
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+        this.setState({orderForm:updatedOrderForm});
+        
+    }
     render(){
+
+        const formElementsArray =[];
+        for(let key in this.state.orderForm){
+            formElementsArray.push({
+                id:key,
+                config:this.state.orderForm[key]
+            })
+        }
+
+
+
 
         let form = (
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name"/>
-                <Input inputtype="input" type="text" name="email" placeholder="Your Mail"/>
-                <Input inputtype="input" type="text" name="street" placeholder="Street"/>
-                <Input inputtype="input" type="text" name="postalcode" placeholder="Postal Code"/>
+                {formElementsArray.map(formElement=>(
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event,formElement.id)}
+                    />
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler} >Place Order</Button>
             </form>
         );
