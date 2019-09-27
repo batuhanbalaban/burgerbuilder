bc7a76d4dc2368252a5eb9b3e0c98beaef9696cf
unknown <batuhan.balaban@yahoo.com>
2019-09-27 19:30:31 +0300

    Guarding Routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import {Route,Switch, withRouter} from 'react-router-dom';
+import {Route,Switch, withRouter, Redirect} from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
@@ -14,16 +14,30 @@ class App extends Component {
     this.props.onTryAutoSignIn();
   }
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth}/>
+        <Route path="/" exact component={BurgerBuilder}/>
+        <Redirect to="/"/>
+      </Switch>
+    );
+
+    if(this.props.isAuthenticated){
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/orders" component={Orders}/>
+          <Route path="/logout" component={Logout}/>
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Redirect to="/"/>
+        </Switch>
+      );
+    }
+
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/orders" component={Orders}/>
-            <Route path="/auth" component={Auth}/>
-            <Route path="/logout" component={Logout}/>
-            <Route path="/" exact component={BurgerBuilder}/>
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
@@ -31,14 +45,12 @@ class App extends Component {
 }
 
 
-// const mapStateToProps = state => {
+const mapStateToProps = state => {
 
-//   return {
-//       ings: state.burgerBuilder.ingredients,
-//       price: state.burgerBuilder.totalPrice,
-//       error: state.burgerBuilder.error
-//   };
-// }
+  return {
+      isAuthenticated: state.auth.token!==null
+  };
+}
 
 const mapDispatchToProps = dispatch =>{
   return {
@@ -46,4 +58,4 @@ const mapDispatchToProps = dispatch =>{
   };
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
