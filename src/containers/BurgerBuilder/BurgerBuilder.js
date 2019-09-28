05217744084601633d523fb5f94f5f203e699ffd
unknown <batuhan.balaban@yahoo.com>
2019-09-28 12:39:12 +0300

    UnitTests added with Enzyme

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import {connect} from 'react-redux';
 import * as actions from '../../store/actions/index';
 
 
-class BurgerBuilder extends Component{
+export class BurgerBuilder extends Component{
 
 
     constructor (props){
---
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,22 @@
+import {BurgerBuilder} from './BurgerBuilder';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import React from 'react';
+import {configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({adapter: new Adapter()});
+
+
+describe('<BurgerBuilder/>',()=>{
+
+    let wrapper;
+
+    beforeEach(()=>{
+        wrapper = shallow(<BurgerBuilder onInitIngredients={()=>{}}/>);
+    });
+
+    it('should render <BuildControls/> when receiving ingredients',()=>{
+        wrapper.setProps({ings:{salad:0}});
+        expect(wrapper.find(BuildControls)).toHaveLength(1);
+    });
+
+});
